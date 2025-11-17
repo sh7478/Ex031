b5ff187a8663f7bc2a06d297a0eb2f5172487300
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-17 11:15:48 +0200

    Added HourlyEmployee

---
+++ app/src/main/java/com/example/ex031/HourlyEmployee.java
@@ -0,0 +1,18 @@
+package com.example.ex031;
+
+public class HourlyEmployee extends Employee{
+
+    private double hoursWorked;
+    private double hourlyRate;
+
+    public HourlyEmployee(int employeeId, double baseSalery, String name, double hoursWorked, double hourlyRate) {
+        super(employeeId, baseSalery, name);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calculateMonthlySalary() {
+        return this.hoursWorked * this.hourlyRate;
+    }
+}
